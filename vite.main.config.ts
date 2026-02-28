54378fd3f35d1109accfc37936d763cb60d8f2d3
Yelaman Abdullin <a.elaman.b@gmail.com>
2026-03-01 00:22:07 +1100

    fix: remove unnecessary external dependency from Vite configuration

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     rollupOptions: {
-      external: ["electron", "electron-store", "canvas"],
+      external: ["electron", "canvas"],
       output: {
         entryFileNames: "main.js",
       },
